50b9c0e44ab442938622d4600fdea50feebddf7b
Vibhor Gulati <vibhorgulati7@gmail.com>
2018-06-28 14:52:43 +0530

    First Commit

---
+++ app/src/main/java/com/example/vibho/registration/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.vibho.registration;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this,Login.class);
+        startActivity(intent);
+    }
+}
